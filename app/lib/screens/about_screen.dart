1fa06d726fbddfa468780d06fb828c3105e81026
Michael Marner <michael@20papercups.net>
2026-01-27 22:25:09 +1030

    deps: replace package info with plus

--- app/lib/screens/about_screen.dart
+++ app/lib/screens/about_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:player/generated/l10n.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -22,9 +22,7 @@ class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(S.of(context).about),
-      ),
+      appBar: AppBar(title: Text(S.of(context).about)),
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.all(24),
@@ -32,8 +30,9 @@ class _AboutScreenState extends State<AboutScreen> {
             Padding(
               padding: const EdgeInsets.only(bottom: 32.0),
               child: Container(
-                  child: Image.asset('assets/images/three_d_logo.png'),
-                  height: 250),
+                child: Image.asset('assets/images/three_d_logo.png'),
+                height: 250,
+              ),
             ),
             Text(S.of(context).aboutBody),
             Padding(
@@ -71,33 +70,33 @@ class _AboutScreenState extends State<AboutScreen> {
                 textAlign: TextAlign.center,
               ),
             ),
-            Text(
-              S.of(context).creditsBody,
-              textAlign: TextAlign.center,
-            ),
+            Text(S.of(context).creditsBody, textAlign: TextAlign.center),
             Padding(
               padding: const EdgeInsets.only(top: 32.0),
               child: FutureBuilder<PackageInfo>(
-                  future: pkgInfo,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Column(
-                        children: [
-                          Text(S.of(context).version(snapshot.data!.version),
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.grey[600])),
-                          Text(
-                            S
-                                .of(context)
-                                .versionBuildNumber(snapshot.data!.buildNumber),
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.grey[600]),
-                          ),
-                        ],
-                      );
-                    }
-                    return CupertinoActivityIndicator();
-                  }),
+                future: pkgInfo,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        Text(
+                          S.of(context).version(snapshot.data!.version),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                        Text(
+                          S
+                              .of(context)
+                              .versionBuildNumber(snapshot.data!.buildNumber),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    );
+                  }
+                  return CupertinoActivityIndicator();
+                },
+              ),
             ),
           ],
         ),
